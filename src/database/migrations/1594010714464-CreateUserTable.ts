36de48c6254b01f87c08197484b6c56bc2f7b880
Gean Gomes <geansg4@gmail.com>
2020-07-06 01:57:47 -0300

    database connected

---
+++ src/database/migrations/1594010714464-CreateUserTable.ts
@@ -0,0 +1,33 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateUserTable1594010714464
+	implements MigrationInterface {
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.createTable(
+			new Table({
+				name: 'users',
+				columns: [
+					{
+						name: 'id',
+						type: 'varchar',
+						generationStrategy: 'uuid',
+						isPrimary: true,
+					},
+					{
+						name: 'email',
+						type: 'varchar',
+						isUnique: true,
+					},
+					{
+						name: 'password',
+						type: 'varchar',
+					},
+				],
+			}),
+		);
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.dropTable('users');
+	}
+}
--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = Router();
 
 routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello, World!' });
+	return response.json({ message: 'Hello, World!' });
 });
 
 export default routes;
